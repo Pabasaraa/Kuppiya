ea9921aae7c8c90dce13e4c6280fb5f0aa0401bc
Pabasaraa <pabasara.was@gmail.com>
2022-03-13 15:19:04 +0530

    Added user login page and connect it with the signup page

--- app/src/main/java/com/example/mad_app/Login.java
+++ app/src/main/java/com/example/mad_app/Login.java
@@ -2,13 +2,33 @@ package com.example.mad_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Login extends AppCompatActivity {
+    Button login, notMember;
+    TextInputLayout username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.login_btn);
+        notMember = findViewById(R.id.not_member);
+        username = findViewById(R.id.username_login);
+        password = findViewById(R.id.password_login);
+
+        notMember.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Signup.class);
+                startActivity(intent);
+            }
+        });
     }
 }

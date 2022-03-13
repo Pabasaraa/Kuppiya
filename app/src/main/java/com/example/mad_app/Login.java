4904af7b8aa7d0f2454ba7ec8dbee3a0658f5686
Pabasaraa <pabasara.was@gmail.com>
2022-03-13 11:45:31 +0530

    Added splash screen and google and lottie dependencies to the project

---
+++ app/src/main/java/com/example/mad_app/Login.java
@@ -0,0 +1,14 @@
+package com.example.mad_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/mad_app/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.mad_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_sreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}

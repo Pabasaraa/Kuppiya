813d9e24dfff870d63b95742eee7db1d9e2cb7fc
Pabasaraa <pabasara.was@gmail.com>
2022-03-14 17:56:08 +0530

    Created user sign up page

--- app/src/main/java/com/example/mad_app/Signup.java
+++ app/src/main/java/com/example/mad_app/Signup.java
@@ -2,13 +2,36 @@ package com.example.mad_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Signup extends AppCompatActivity {
+
+    Button signup, alreadyMember;
+    TextInputLayout name, username, email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        signup = findViewById(R.id.signup_btn);
+        alreadyMember = findViewById(R.id.already_member);
+        name = findViewById(R.id.name_signup);
+        username = findViewById(R.id.username_signup);
+        email = findViewById(R.id.email_signup);
+        password = findViewById(R.id.password_signup);
+
+        alreadyMember.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }

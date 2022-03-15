3168352c662af2ab161fbb9469b23e87663e8173
Pabasaraa <pabasara.was@gmail.com>
2022-03-15 13:54:28 +0530

    Created dashboard with bottom navigation view

---
+++ app/src/main/java/com/example/mad_app/Dashboard.java
@@ -0,0 +1,16 @@
+package com.example.mad_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+}
--- app/src/main/java/com/example/mad_app/Signup.java
+++ app/src/main/java/com/example/mad_app/Signup.java
@@ -33,5 +33,13 @@ public class Signup extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+                startActivity(intent);
+            }
+        });
     }
 }

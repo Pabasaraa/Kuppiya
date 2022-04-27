d097dfcd9138fc13fa4b3ac939965c927b17f366
Pabasaraa <pabasara.was@gmail.com>
2022-04-27 15:52:20 +0530

    fixed a minor issue

--- app/src/main/java/com/example/mad_app/AddFragment.java
+++ app/src/main/java/com/example/mad_app/AddFragment.java
@@ -1,12 +1,11 @@
 package com.example.mad_app;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
